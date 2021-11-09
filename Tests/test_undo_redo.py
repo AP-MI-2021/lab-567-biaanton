3473b517f4ff13719a593bc9f4ddaeffae1e8ce9
Bia Anton <biancabub@yahoo.com>
2021-11-09 21:17:56 +0200

    Laborator567

--- Tests/test_undo_redo.py
+++ Tests/test_undo_redo.py
@@ -3,15 +3,6 @@ from Logic.crud import read, delete, create
 from Logic.undo_redo import do_undo, do_redo
 
 
-def get_data():
-    return [creeaza_obiect(1, 'o1', 'desc1', 100, 'LOC1'),
-            creeaza_obiect(2, 'o2', 'desc2', 60, 'LOC1'),
-            creeaza_obiect(3, 'o3', 'desc3', 10, 'LOC3'),
-            creeaza_obiect(4, 'o4', 'desc4', 145421, 'LOC4'),
-            creeaza_obiect(5, 'o5', 'desc5', 20.87, 'LOC4')
-            ]
-
-
 def test_undo_redo():
         obiecte = []
         undo_list = []
@@ -24,20 +15,25 @@ def test_undo_redo():
         assert len(obiecte) == 1
         obiecte = do_undo(undo_list, redo_list, obiecte)
         assert len(obiecte) == 0
+        assert do_undo(undo_list, redo_list, obiecte) is None
+        assert len(obiecte) == 0
         obiecte = create(obiecte, 1, 'o1', 'desc1', 100, 'LOC1', undo_list, redo_list)
         obiecte = create(obiecte, 2, 'o2', 'desc2', 60, 'LOC1', undo_list, redo_list)
         obiecte = create(obiecte, 3, 'o3', 'desc3', 10, 'LOC3', undo_list, redo_list)
         obiecte = do_redo(undo_list, redo_list, obiecte)
+        assert do_redo(undo_list, redo_list, obiecte) is None
         obiecte = do_undo(undo_list, redo_list, obiecte)
         obiecte = do_undo(undo_list, redo_list, obiecte)
         assert len(obiecte) == 1
         obiecte = do_redo(undo_list, redo_list, obiecte)
         assert len(obiecte) == 2
         obiecte = do_redo(undo_list, redo_list, obiecte)
+        assert do_redo(undo_list, redo_list, obiecte) is None
         obiecte = do_undo(undo_list, redo_list, obiecte)
         obiecte = do_undo(undo_list, redo_list, obiecte)
         assert len(obiecte) == 1
         obiecte = create(obiecte, 4, 'o4', 'desc4', 145421, 'LOC4' , undo_list, redo_list)
+        assert len(obiecte) == 2
         obiecte = do_redo(undo_list, redo_list, obiecte)
         obiecte = do_undo(undo_list, redo_list, obiecte)
         assert len(obiecte) == 1
@@ -45,6 +41,7 @@ def test_undo_redo():
         assert len(obiecte) == 0
         obiecte = do_redo(undo_list, redo_list, obiecte)
         obiecte = do_redo(undo_list, redo_list, obiecte)
+        assert do_redo(undo_list, redo_list, obiecte) is None
 
 
 
